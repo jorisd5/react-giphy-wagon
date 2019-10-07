12d175d88a21c012c6b1bb3128a6c07d9a9df05c
Joris Dewaele <joris.dewaele@tutanota.com>
2019-10-07 16:56:21 +0200

    Exercises React rendering

--- src/components/gif.jsx
+++ src/components/gif.jsx
@@ -4,13 +4,24 @@
 import React, { Component } from 'react';
 
 class Gif extends Component {
+  shouldComponentUpdate(nextProps) {
+    return nextProps.id !== this.props.id;
+  }
+
   handleClick = () => {
     if (this.props.selectGif) {
       this.props.selectGif(this.props.id);
     }
   }
 
+
   render() {
+    console.log(`SEARCH_BAR RENDER ${this.props.id}`);
+    const { id } = this.props;
+    if (!id) {
+      return null;
+    }
+
     const src = `https://media2.giphy.com/media/${this.props.id}/200.gif`;
     return (
       <img src={src} className="gif" onClick={this.handleClick} />
--- src/components/search_bar.jsx
+++ src/components/search_bar.jsx
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
+  // Er worden geen props gebruikt in de render method
+  shouldComponentUpdate() {
+    return false;
+  }
+
   handleUpdate = (event) => {
     this.props.searchFunction(event.target.value);
   }
 
   render() {
+    console.log("SEARCH_BAR RENDER ");
     return (
       <input
         type="text"
